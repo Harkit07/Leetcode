48f3edde03e5c2dd1bfa19cf562bc1f6ce379909
HARKIT SINGH SRAN <harkitsinghsran9584@gmail.com>
2026-08-22 08:18:42 +0530

    Time: 0 ms (100%), Space: 42.9 MB (67.95%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,27 +14,17 @@
  * }
  */
 class Solution {
-    static StringBuilder count=new StringBuilder();
-    static int sum=0;
-
-    public void countSum(TreeNode root){
-         if(root==null){
-            return;
-        }
-        count.append(root.val);
-        if(root.left==null && root.right==null){
-            int num = Integer.parseInt(count.toString());
-            sum+=num;
-        }
-        countSum(root.left);
-        countSum(root.right);
-        count.deleteCharAt(count.length()-1);
-    }
-
     public int sumNumbers(TreeNode root) {
-        sum=0;
-        count=new StringBuilder("");
-        countSum(root);
-        return sum;
+        return dfs(root, 0);
+    }
+    private int dfs(TreeNode root, int curr){
+        if(root==null){
+            return 0;
+        }
+        curr=(curr*10)+root.val;
+        if(root.left==null && root.right ==null){
+            return curr;
+        }
+        return dfs(root.left, curr)+dfs(root.right, curr);
     }
 }
